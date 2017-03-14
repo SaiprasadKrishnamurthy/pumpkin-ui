2ef5e8e66b43778d951a2891e542c1fff91a77d1
saipkri <saiprkri@cisco.com>
2017-03-14 10:59:58 +0530

    experimental version of snapshot collection

--- src/main/java/com/sai/pumpkin/model/MavenGitVersionMapping.java
+++ src/main/java/com/sai/pumpkin/model/MavenGitVersionMapping.java
@@ -3,15 +3,23 @@ package com.sai.pumpkin.model;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by saipkri on 07/03/17.
  */
 @Data
 public class MavenGitVersionMapping implements Serializable {
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMMyy'@'HH:mm");
     public String id;
     private ArtifactConfig artifactConfig;
     private MavenCoordinates mavenCoordinates;
     private String gitRevision;
     private long timestamp;
+    private String displayDate;
+
+    public String getDisplayDate() {
+        return dateFormat.format(new Date(timestamp));
+    }
 }
