a51f3bed8e2961a65e46a77262e4048b1bbb4331
Sai Kris <trojan@dodgyworld.com>
2017-04-07 07:16:32 +0530

    change magnitude top 30

--- src/main/java/com/sai/pumpkin/controller/ReleaseSummaryController.java
+++ src/main/java/com/sai/pumpkin/controller/ReleaseSummaryController.java
@@ -147,8 +147,9 @@ public class ReleaseSummaryController {
         ChartSeries filesChanged = new ChartSeries();
         filesChanged.setLabel("# of files changed");
 
-        Collections.sort(currentDiff.getDiffs(), (a, b) -> Long.valueOf(a.getNoOfFilesChanged()).compareTo(b.getNoOfFilesChanged()));
-        List<GitLogSummaryResponse> topN = currentDiff.getDiffs().stream().limit(30L).collect(Collectors.toList());
+        List<GitLogSummaryResponse> topN = currentDiff.getDiffs().stream().limit(30L)
+                .sorted((a, b) -> Long.valueOf(b.getNoOfFilesChanged()).compareTo(a.getNoOfFilesChanged()))
+                .collect(Collectors.toList());
         System.out.println(topN);
 
         for (GitLogSummaryResponse b : topN) {
